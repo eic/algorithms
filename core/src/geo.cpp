a3392cb9ecb4d4125498b1b55ddca5f099d52ce1
Wouter Deconinck <wdconinc@gmail.com>
2023-12-31 12:40:41 -0600

    fix: switch to GeoSvc::init(const dd4hep::Detector*)
    This implements GeoSvc::init(const dd4hep::Detector*), which requires
    storing the xml geometry in a unique_ptr, creating it non-const, moving
    it, and handing out const get()'s to it.

--- core/src/geo.cpp
+++ core/src/geo.cpp
@@ -7,23 +7,25 @@
 
 namespace algorithms {
 
-void GeoSvc::init(dd4hep::Detector* det) {
+void GeoSvc::init(const dd4hep::Detector* det) {
   if (det) {
     info() << "Initializing geometry service from pre-initialized detector" << endmsg;
     m_detector = det;
     // no detector given, need to self-initialize
   } else {
     info() << "No external detector provided, self-initializing" << endmsg;
-    m_detector = &(dd4hep::Detector::getInstance());
+    auto detector = dd4hep::Detector::make_unique("");
     if (m_xml_list.empty()) {
       // TODO handle error
     }
     for (std::string_view name : m_xml_list) {
       info() << fmt::format("Loading compact file: {}", "name") << endmsg;
-      m_detector->fromCompact(std::string(name));
+      detector->fromCompact(std::string(name));
     }
-    m_detector->volumeManager();
-    m_detector->apply("DD4hepVolumeManager", 0, nullptr);
+    detector->volumeManager();
+    detector->apply("DD4hepVolumeManager", 0, nullptr);
+    m_detector_ptr = std::move(detector);
+    m_detector = m_detector_ptr.get();
   }
   // always: instantiate cellIDConverter
   m_converter = std::make_unique<const dd4hep::rec::CellIDPositionConverter>(*m_detector);
