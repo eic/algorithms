5c89a34b7d4f59c4400d8d365099804206242253
Wouter Deconinck <wdconinc@gmail.com>
2025-04-25 15:51:48 -0500

    feat: move protected report_fmt to public log function (#22)

--- core/include/algorithms/logger.h
+++ core/include/algorithms/logger.h
@@ -192,22 +192,22 @@ protected:
   void trace(std::string_view msg) const { report<LogLevel::kTrace>(msg); }
 
   template <typename ...T> constexpr void critical(fmt::format_string<T...> fmt, T&&... args) const {
-    report_fmt<LogLevel::kCritical>(fmt, std::forward<decltype(args)>(args)...);
+    log<LogLevel::kCritical>(fmt, std::forward<decltype(args)>(args)...);
   }
   template <typename ...T> constexpr void error(fmt::format_string<T...> fmt, T&&... args) const {
-    report_fmt<LogLevel::kError>(fmt, std::forward<decltype(args)>(args)...);
+    log<LogLevel::kError>(fmt, std::forward<decltype(args)>(args)...);
   }
   template <typename ...T> constexpr void warning(fmt::format_string<T...> fmt, T&&... args) const {
-    report_fmt<LogLevel::kWarning>(fmt, std::forward<decltype(args)>(args)...);
+    log<LogLevel::kWarning>(fmt, std::forward<decltype(args)>(args)...);
   }
   template <typename ...T> constexpr void info(fmt::format_string<T...> fmt, T&&... args) const {
-    report_fmt<LogLevel::kInfo>(fmt, std::forward<decltype(args)>(args)...);
+    log<LogLevel::kInfo>(fmt, std::forward<decltype(args)>(args)...);
   }
   template <typename ...T> constexpr void debug(fmt::format_string<T...> fmt, T&&... args) const {
-    report_fmt<LogLevel::kDebug>(fmt, std::forward<decltype(args)>(args)...);
+    log<LogLevel::kDebug>(fmt, std::forward<decltype(args)>(args)...);
   }
   template <typename ...T> constexpr void trace(fmt::format_string<T...> fmt, T&&... args) const {
-    report_fmt<LogLevel::kTrace>(fmt, std::forward<decltype(args)>(args)...);
+    log<LogLevel::kTrace>(fmt, std::forward<decltype(args)>(args)...);
   }
 
   bool aboveCriticalThreshold() const { return m_level >= LogLevel::kCritical; }
@@ -231,14 +231,14 @@ protected:
     return os;
   }
 
-private:
   template <LogLevel l, typename ...T>
-  constexpr void report_fmt(fmt::format_string<T...> fmt, T&&... args) const {
+  constexpr void log(fmt::format_string<T...> fmt, T&&... args) const {
     if (l >= m_level) {
       m_logger.report(l, m_caller, fmt::format(fmt, std::forward<decltype(args)>(args)...));
     }
   }
 
+private:
   template <LogLevel l> void report(std::string_view msg) const {
     if (l >= m_level) {
       m_logger.report(l, m_caller, msg);
